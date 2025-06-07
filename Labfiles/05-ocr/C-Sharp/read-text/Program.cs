f311895cea18f00dca1e5075a1ed814acf77cd8b
Paul McNamara <paulmac445@gmail.com>
2025-06-07 17:53:03 -0400

    Image detection and tagging

--- Labfiles/05-ocr/C-Sharp/read-text/Program.cs
+++ Labfiles/05-ocr/C-Sharp/read-text/Program.cs
@@ -7,6 +7,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Azure;
+using Azure.AI.Vision.ImageAnalysis;
 
 // Import namespaces
 
@@ -27,7 +28,7 @@ namespace read_text
                 string aiSvcKey = configuration["AIServicesKey"];
 
                 // Authenticate Azure AI Vision client
-
+                ImageAnalysisClient client = new(new Uri(aiSvcEndpoint), new AzureKeyCredential(aiSvcKey));
 
                 // Menu for text reading functions
                 Console.WriteLine("\n1: Use Read API for image (Lincoln.jpg)\n2: Read handwriting (Note.jpg)\nAny other key to quit\n");
@@ -60,12 +61,36 @@ namespace read_text
         {
             Console.WriteLine($"\nReading text from {imageFile} \n");
 
-            // Use a file stream to pass the image data to the analyze call
-            using FileStream stream = new FileStream(imageFile,
-                                                     FileMode.Open);
+            // // Use a file stream to pass the image data to the analyze call
+            // using FileStream stream = new FileStream(imageFile,
+            //                                          FileMode.Open);
 
             // Use Analyze image function to read text in image
-            
+            using FileStream stream = new FileStream(imageFile, FileMode.Open);
+            Console.WriteLine($"\nReading text from {imageFile} \n");
+
+            ImageAnalysisResult result = client.Analyze(BinaryData.FromStream(stream), VisualFeatures.Read);
+
+            stream.Close();
+
+            if (result.Read != null)
+            {
+                Console.WriteLine($"Text:");
+                 // Prepare image for drawing
+                System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                Graphics graphics = Graphics.FromImage(image);
+                Pen pen = new Pen(Color.Cyan, 3);
+                
+                foreach (var line in result.Read.Blocks.SelectMany(block => block.Lines))
+                {
+                    Console.WriteLine($" {line.Text}");
+                }
+
+               // Save image
+                String output_file = "text.jpg";
+                image.Save(output_file);
+                Console.WriteLine("\nResults saved in " + output_file + "\n");   
+            }
     
         }
     }
