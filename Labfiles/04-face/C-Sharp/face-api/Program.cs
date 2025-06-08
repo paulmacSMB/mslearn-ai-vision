533c9444d8ede2c174c1e6b09b0d18744c2aa42f
Paul McNamara <paulmac445@gmail.com>
2025-06-07 20:16:03 -0400

    Face Resource

--- Labfiles/04-face/C-Sharp/face-api/Program.cs
+++ Labfiles/04-face/C-Sharp/face-api/Program.cs
@@ -5,6 +5,9 @@ using System.Drawing;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Azure.AI.Vision.Face;
+using Azure;
+using System.Runtime.CompilerServices;
 
 // Import namespaces
 
@@ -27,8 +30,8 @@ namespace analyze_faces
                 string cogSvcKey = configuration["AIServiceKey"];
 
                 // Authenticate Face client
-
-
+                FaceClient faceClient = new(new Uri(cogSvcEndpoint), new AzureKeyCredential(cogSvcKey));
+                
                 // Menu for face functions
                 Console.WriteLine("1: Detect faces\nAny other key to quit");
                 Console.WriteLine("Enter a number:");
@@ -36,7 +39,17 @@ namespace analyze_faces
                 switch (command)
                 {
                     case "1":
-                        await DetectFaces("images/people.jpg");
+                        await DetectFaces("images/people.jpg", faceClient);
+                        break;
+                    case "2":
+                        await DetectFaces("images/people2.jpg", faceClient);
+                        break;
+                    case "3":
+                        Console.WriteLine("made it to switch");
+                        await DetectFaces("images/person1.jpg", faceClient);
+                        break;
+                    case "4":
+                        await DetectFaces("images/person2.jpg", faceClient);
                         break;
                     default:
                         break;
@@ -48,16 +61,76 @@ namespace analyze_faces
             }
         }
 
-        static async Task DetectFaces(string imageFile)
+        static async Task DetectFaces(string imageFile, FaceClient faceClient)
         {
             Console.WriteLine($"Detecting faces in {imageFile}");
 
             // Specify facial features to be retrieved
-
+            FaceAttributeType[] features = new FaceAttributeType[]
+            {
+                FaceAttributeType.Detection01.HeadPose,
+                FaceAttributeType.Detection01.Occlusion,
+                FaceAttributeType.Detection01.Accessories
+            };
 
             // Get faces
- 
- 
+            Console.WriteLine($"DetectFaces: {imageFile}");
+            using (var imageData = File.OpenRead(imageFile))
+            {
+                Console.WriteLine($"inside using {faceClient}");
+                Console.WriteLine($"imagedata object { imageData.Length }");
+                var response = await faceClient.DetectAsync(
+                    BinaryData.FromStream(imageData),
+                    FaceDetectionModel.Detection01,
+                    FaceRecognitionModel.Recognition01,
+                    returnFaceId: false,
+                    returnFaceAttributes: features);
+                IReadOnlyList<FaceDetectionResult> detected_faces = response.Value;
+
+                Console.WriteLine("after faceClient gets upped");
+                if (detected_faces.Count() > 0)
+                {
+                    Console.WriteLine($"{detected_faces.Count()} faces detected.");
+
+                    // Prepare image for drawing
+                    Image image = Image.FromFile(imageFile);
+                    Graphics graphics = Graphics.FromImage(image);
+                    Pen pen = new Pen(Color.LightGreen, 3);
+                    Font font = new Font("Arial", 4);
+                    SolidBrush brush = new SolidBrush(Color.White);
+                    int faceCount = 0;
+
+                    // Draw and annotate each face
+                    foreach (var face in detected_faces)
+                    {
+                        faceCount++;
+                        Console.WriteLine($"\nFace number {faceCount}");
+
+                        // Get face properties
+                        Console.WriteLine($" - Head Pose (Yaw): {face.FaceAttributes.HeadPose.Yaw}");
+                        Console.WriteLine($" - Head Pose (Pitch): {face.FaceAttributes.HeadPose.Pitch}");
+                        Console.WriteLine($" - Head Pose (Roll): {face.FaceAttributes.HeadPose.Roll}");
+                        Console.WriteLine($" - Forehead occluded: {face.FaceAttributes.Occlusion.ForeheadOccluded}");
+                        Console.WriteLine($" - Eye occluded: {face.FaceAttributes.Occlusion.EyeOccluded}");
+                        Console.WriteLine($" - Mouth occluded: {face.FaceAttributes.Occlusion.MouthOccluded}");
+
+                        // Draw and annotate face
+                        var r = face.FaceRectangle;
+                        Rectangle rect = new Rectangle(r.Left, r.Top, r.Width, r.Height);
+                        graphics.DrawRectangle(pen, rect);
+                        string annotation = $"Face number {faceCount}";
+                        graphics.DrawString(annotation, font, brush, r.Left, r.Top);
+                        
+                    }
+
+                    // Save annotated image
+                    String output_file = "detected_faces.jpg";
+                    image.Save(output_file);
+                    Console.WriteLine(" Results saved in " + output_file);
+                }
+            }
+
+
         }
     }
 }
